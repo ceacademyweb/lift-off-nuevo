70c52d68ea332140d46bb0ec0450707b76d338f0
Florentino Navarro <ceacademy.web@gmail.com>
2023-04-01 20:23:05 -0600

    first commit

--- src/utils/apiPath.ts
+++ src/utils/apiPath.ts
@@ -1,2 +1,2 @@
-export const API_PATH = 'http://localhost:5000';
-// export const API_PATH = 'https://ceacademy-auth-production.up.railway.app';
+// export const API_PATH = 'http://localhost:5000';
+export const API_PATH = 'https://ceacademy-auth-production.up.railway.app';
